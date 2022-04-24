9576bb13e67c760b58e8603b382bc2b3871bdf1c
Dungoss <leduydung0123@gmail.com>
2022-04-24 13:30:00 +0700

    build: FE

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ app.use(cors());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'admin/build')));
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -50,7 +52,9 @@ app.get("/logout", async (req, res) => {
     });
   }
 });
-
+app.get('/*', function(req, res, next){
+  res.sendFile(path.join(__dirname, './admin/build/index.html'))
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
